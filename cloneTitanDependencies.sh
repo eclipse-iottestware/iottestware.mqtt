b6d641dd51118f144b5f766cc5f2b41c9dd57667
Alexander Kaiser <alexander.kaiser@relayr.io>
2017-08-28 14:26:04 +0200

    extended cloneTitanDependencies.sh to cope with updates in Titan repositories

--- cloneTitanDependencies.sh
+++ cloneTitanDependencies.sh
@@ -29,13 +29,27 @@ PATH_MQTT=${PATH_BASE}/ProtocolModules/MQTT/
 PATH_COMMON=${PATH_BASE}/ProtocolModules/COMMON/
 PATH_TCC=${PATH_BASE}/Libraries/TCCUsefulFunctions_CNL113472/
 
+# Git flags
+GIT_QUIET=-q # remove this flag for complete Git output
+
 ##############
 # Titan.Core #
 ##############
 if [ -d "$PATH_CORE" ]; then
   echo $PATH_CORE " aready exists"
+
+  # goto the directory 
+  cd $PATH_CORE
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
-  git clone https://github.com/eclipse/titan.core.git $PATH_CORE
+  git clone $GIT_QUIET https://github.com/eclipse/titan.core.git $PATH_CORE
 fi
 
 #############
@@ -43,12 +57,34 @@ fi
 #############
 if [ -d "$PATH_SOCKET_API" ]; then
   echo $PATH_SOCKET_API " already exists"
+
+  # goto the directory 
+  cd $PATH_SOCKET_API
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone https://github.com/eclipse/titan.TestPorts.Common_Components.Socket-API.git $PATH_SOCKET_API
 fi
 
 if [ -d "$PATH_IPL4" ]; then
   echo $PATH_IPL4 " already exists"
+
+  # goto the directory 
+  cd $PATH_IPL4
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone https://github.com/eclipse/titan.TestPorts.IPL4asp.git $PATH_IPL4
 fi
@@ -58,24 +94,68 @@ fi
 ###################
 if [ -d "$PATH_JSON" ]; then
   echo $PATH_JSON " already exists"
+
+  # goto the directory 
+  cd $PATH_JSON
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone https://github.com/eclipse/titan.ProtocolModules.JSON_v07_2006.git $PATH_JSON
 fi
 
 if [ -d "$PATH_COAP" ]; then
   echo $PATH_COAP " already exists"
+
+  # goto the directory 
+  cd $PATH_COAP
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone git://git.eclipse.org/gitroot/titan/titan.ProtocolModules.CoAP.git $PATH_COAP
 fi
 
 if [ -d "$PATH_MQTT" ]; then
   echo $PATH_MQTT " already exists"
+
+  # goto the directory 
+  cd $PATH_MQTT
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone git://git.eclipse.org/gitroot/titan/titan.ProtocolModules.MQTT.git $PATH_MQTT
 fi
 
 if [ -d "$PATH_COMMON" ]; then
   echo $PATH_COMMON " already exists"
+
+  # goto the directory 
+  cd $PATH_COMMON
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone https://github.com/eclipse/titan.ProtocolModules.COMMON.git $PATH_COMMON
 fi
@@ -85,6 +165,17 @@ fi
 #############
 if [ -d "$PATH_TCC" ]; then
   echo $PATH_TCC " already exists"
+
+  # goto the directory 
+  cd $PATH_TCC
+
+  # make sure you have the latest codebase
+  git remote update
+  git checkout  $GIT_QUIET master
+  git pull $GIT_QUIET
+ 
+  # return back to the last known folder
+  cd - > /dev/null
 else
   git clone https://github.com/eclipse/titan.Libraries.TCCUsefulFunctions.git $PATH_TCC
 fi
@@ -103,10 +194,10 @@ fi
 cd bin
 
 # link to MQTT ProtocolModule
-ln -s ${PATH_MQTT}/src/negative_testing/Mqtt_v3.1.1_Types.ttcn
-ln -s ${PATH_MQTT}/src/negative_testing/Mqtt_v3_1_1_IPL4SizeFunction.ttcn
-ln -s ${PATH_MQTT}/src/negative_testing/Mqtt_v3.1.1_EncDec.cc
-ln -s ${PATH_MQTT}/src/negative_testing/Mqtt_v3.1.1_Size.cc
+ln -s ${PATH_MQTT}/src/negative_testing/MQTT_v3_1_1_Types.ttcn
+ln -s ${PATH_MQTT}/src/negative_testing/MQTT_v3_1_1_IPL4SizeFunction.ttcn
+ln -s ${PATH_MQTT}/src/negative_testing/MQTT_v3_1_1_EncDec.cc
+ln -s ${PATH_MQTT}/src/negative_testing/MQTT_v3_1_1_Size.cc
 
 # link to IPL4asp TestPort
 ln -s ${PATH_IPL4}/src/IPL4asp_Types.ttcn
