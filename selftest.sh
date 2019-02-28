d79e09ed3d973881fc24b09b4777e9e52bf775fe
Alexander Kaiser <alexander.kaiser@relayr.io>
2019-02-28 14:41:02 +0100

    Fixed erroneous decodings. MQTTasp Port can now use both decoders. Extended self-test for this purpose

--- selftest.sh
+++ selftest.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# choos config 01 by default, else config 02
+if [ $# -eq 0 ]; then
+    echo "Self-Test Configuration 01"
+    CONFIG="01"
+else
+    echo "Self-Test Configuration 02"
+    CONFIG="02"
+fi
+
 echo "Start Broker"
-ttcn3_start ./bin/iottestware.mqtt ./cfg/self_test/st_broker_01.cfg &
+ttcn3_start ./bin/iottestware.mqtt ./cfg/self_test/st_broker_$CONFIG.cfg &
 
 echo "Start Client"
-# grep will show us only the statistics of the client -> | grep "Verdict statistics:"
-ttcn3_start ./bin/iottestware.mqtt ./cfg/self_test/st_client_01.cfg | grep "Verdict statistics:"
+ttcn3_start ./bin/iottestware.mqtt ./cfg/self_test/st_client_$CONFIG.cfg
